b1b6dc1d1b1ea4bcbceee5c567f16ee78524b91f
juangomez2005 <juan.gcuellar@mail.escuelaing.edu.co>
2026-04-09 19:21:20 -0500

    Preparacion de ambiente

---
+++ ECI-SportLife/src/main/java/edu/dosw/parcial/ECI_SportLife/EciSportLifeApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.parcial.ECI_SportLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EciSportLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EciSportLifeApplication.class, args);
+	}
+
+}
---
+++ ECI-SportLife/src/test/java/edu/dosw/parcial/ECI_SportLife/EciSportLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.parcial.ECI_SportLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EciSportLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
